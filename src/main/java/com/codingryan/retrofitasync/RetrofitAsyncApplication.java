e3c9f08d1fb6f104f55af9d5a71f0d49403ef6b6
munjisu <munjisu@gmail.com>
2023-04-18 08:32:11 +0900

    initial commit

---
+++ src/main/java/com/codingryan/retrofitasync/RetrofitAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.codingryan.retrofitasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrofitAsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingryan/retrofitasync/RetrofitAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingryan.retrofitasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitAsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
